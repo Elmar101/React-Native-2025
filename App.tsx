2de044c3840cf55805e0bc551d7ff6deaa511056
elmar alibeyli <elmar.amanov.2015@mail.ru>
2025-07-07 01:46:07 +0400

    feat: use Dimensions and Button TouchableOpacity

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const { width, height } = Dimensions.get("window");
+  console.log("Window dimensions:", { width, height });
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-       <Text>Open up App.tsx to start working on your app!</Text>
-       <Button title="Press Me" />
+      <Text style={styles.text}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <Text style={{ borderWidth: 2, width: width * 0.9 }}>Open up </Text>
+      <Text>Open </Text>
+      <Button title="Press Me" />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Press Me TouchableOpacity</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,30 +30,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: '#333',
+    color: "#333",
     fontSize: 20,
   },
   button: {
-    backgroundColor: '#007BFF',
+    backgroundColor: "#007BFF",
     padding: 10,
     borderRadius: 5,
+    marginVertical: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
-    textAlign: 'center',
+    textAlign: "center",
   },
   input: {
     height: 40,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderWidth: 1,
     paddingHorizontal: 10,
     marginVertical: 10,
-    width: '80%',
+    width: "80%",
   },
 });

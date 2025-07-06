4bd64d20513366e77fd5085d1ce6557d4c6ed3c4
elmar alibeyli <elmar.amanov.2015@mail.ru>
2025-07-06 19:01:17 +0400

    Configurasia

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+       <Text>Open up App.tsx to start working on your app!</Text>
+       <Button title="Press Me" />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#333',
+    fontSize: 20,
+  },
+  button: {
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+    width: '80%',
   },
 });
